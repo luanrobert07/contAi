cf6a10d281545052ea60f42f4ca77ccaca8cb941
luanrobert07 <luan.robert@gec.inatel.br>
2025-06-03 23:26:58 +0200

    chore: update project configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express"
 import { myDataSource } from "./app-data-source"
 
@@ -15,14 +14,3 @@ const app = express()
 app.use(express.json())
 
 app.listen(3000)
-=======
-import 'reflect-metadata';
-import express from 'express';
-
-
-const app = express();
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
->>>>>>> 8ab6821b92a0fe2447ff3a7334a3103afd7cb80a
